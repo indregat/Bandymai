e32c1d3e7f97c3acb4c7366bd10e7e787c64fcd7
indre.gatulyte <indre.gatulyte@gmail.com>
2019-12-20 15:35:39 +0200

    data set

---
+++ js/data.js
@@ -0,0 +1,122 @@
+"use strict";
+
+const post = [
+    {
+        author: {
+            name: 'Chirstine',
+            surname: 'Dear',
+            img: 'christine.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis.'
+        }
+    },
+    {
+        author: {
+            name: 'Dwight',
+            surname: 'Dear',
+            img: 'dwight.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis.'
+        }
+    },
+    {
+        author: {
+            name: 'Dylan',
+            surname: 'Dear',
+            img: 'dylan.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            img: ['img-1.png']
+        }
+    },
+    {
+        author: {
+            name: 'Jack',
+            surname: 'Dear',
+            img: 'jack.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            img: ['img-1.png', 'img-2.png']
+        }
+    },
+    {
+        author: {
+            name: 'Olivia',
+            surname: 'Dear',
+            img: 'olivia.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            img: ['img-1.png', 'img-2.png', 'img-3.jpg']
+        }
+    },
+    {
+        author: {
+            name: 'Robert',
+            surname: 'Dear',
+            img: 'robert.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            img: ['img-1.png', 'img-2.png', 'img-3.jpg', 'img-4.png']
+        }
+    },
+    {
+        author: {
+            name: 'Robert',
+            surname: 'Dear',
+            img: 'robert.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            img: ['img-1.png', 'img-2.png', 'img-3.jpg', 'img-4.png', 'img-2.png', 'img-3.jpg', 'img-4.png']
+        }
+    },
+    {
+        author: {
+            name: 'Olivia',
+            surname: 'Dear',
+            img: 'olivia.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        }
+    },
+    {
+        author: {
+            name: 'Dylan',
+            surname: 'Dear',
+            img: 'dylan.jpg',
+            link: '#'
+        },
+        time: 1576592401104,
+        content: {
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            background: 'red'
+        }
+    }
+];
+
+//console.log(post[0][pavarde]); parasytu pirmojo posto [0]- yra pirmas postas. 
+// 
+console.log(post);
