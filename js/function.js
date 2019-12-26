a2f0d54aeff7429a610820c90cce6935dafc6652
indre.gatulyte <indre.gatulyte@gmail.com>
2019-12-26 13:43:12 +0200

    post template #3

---
+++ js/function.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function renderFeed(list) {
+    console.log(list);
+    
+    for (let i=0; i<list.length; i++){
+        
+    
+    console.log('render single post');
+    }
+    
+    return;
+}
+
